68435b26566059de79403b2912894467f11c96d7
szhayes <51539583+szhayes@users.noreply.github.com>
2021-08-26 14:35:54 -0400

    update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "default" {
   name         = "my-first-instance"
-  machine_type = "e2-micro"
+  machine_type = "e2-small"
   zone         = "us-central1-a"
 
   boot_disk {
